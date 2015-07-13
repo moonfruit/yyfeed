edf213baff93850f69021d094935e42d30612011
MoonFruit <dkmoonfruit@gmail.com>
2015-07-13 22:49:43 +0800

    add feed database and generator

--- lib/yyfeed/app/feed.py
+++ lib/yyfeed/app/feed.py
@@ -3,18 +3,18 @@
 
 from datetime import datetime, timedelta
 
-from bottle import Bottle, abort, default_app, request, route
+from bottle import Bottle, abort, default_app, request, response, route
 
 from ..db import Feed, FeedItem
 from ..feed import Atom1Feed
 
-
 __all__ = ['app']
-
 
 app = Bottle()
 with app:
     assert app is default_app()
+
+    ITEM_MAX_NUM = 100
 
     # noinspection PyUnresolvedReferences
     @route('/')
@@ -27,7 +27,7 @@ with app:
             abort(404, 'No such feed [%s].' % id)
 
         atom = Atom1Feed(
-            id=feed.id,
+            id=request.url,
             title=feed.title,
             link=feed.link,
             description=feed.description,
@@ -35,9 +35,11 @@ with app:
 
         for feedItem in (
                 db.query(FeedItem)
-                        .filter_by(feed_id = id)
-                        .filter(FeedItem.datetime >= datetime.today() - days)
-                        .order_by(FeedItem.datetime.desc())
+                        .filter_by(feed_id=id)
+                        .filter((FeedItem.datetime == None) |
+                                    (FeedItem.datetime >= datetime.today() - days))
+                        .order_by(FeedItem.datetime.desc(), FeedItem.id.desc())
+                        .limit(ITEM_MAX_NUM)
         ):
             atom.add_item(
                 title=feedItem.title,
